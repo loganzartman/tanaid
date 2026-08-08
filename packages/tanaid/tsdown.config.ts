d5acddc5aaffcdb4cf12e08c91d5a616a66db510
Logan <loganzartman@live.com>
2026-08-07 20:52:14 -0700

    emit dts

--- packages/tanaid/tsdown.config.ts
+++ packages/tanaid/tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsdown'
 import { wasm } from 'rolldown-plugin-wasm';
 
 export default defineConfig({
+  dts: true,
   entry: ['./src/index.ts'],
   plugins: [wasm()],
 });
