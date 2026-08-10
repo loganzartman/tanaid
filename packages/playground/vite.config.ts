207ffaca3191ee001a62305569af0cf559e93432
Logan <logan.zartman@utexas.edu>
2026-08-10 02:34:22 -0700

    fix targets

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 
+const target = ["chrome132", "edge132", "firefox134", "safari18.2"];
+
 export default defineConfig({
+  build: { target },
   worker: {
     format: "es",
   },
