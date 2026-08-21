10d0b2f82be50e0406565fbe2c33163df0ac49cd
Logan <logan.zartman@utexas.edu>
2026-08-21 13:39:00 -0700

    add createInterpreter

--- packages/tanaid/src/index.ts
+++ packages/tanaid/src/index.ts
@@ -1,1 +1,36 @@
+import {
+  Interpreter,
+  InterpreterOptions,
+} from "../../../crates/tanaid-wasm/pkg/bundler/tanaid_wasm";
 export * from "../../../crates/tanaid-wasm/pkg/bundler/tanaid_wasm";
+
+export type CreateInterpreterOptions = Omit<
+  InterpreterOptions,
+  "handleStdout" | "handleEventLoopStatus" | "setTimeout" | "clearTimeout"
+> & {
+  handleStdout?: InterpreterOptions["handleStdout"];
+  handleEventLoopStatus?: InterpreterOptions["handleEventLoopStatus"];
+  setTimeout?: InterpreterOptions["setTimeout"];
+  clearTimeout?: InterpreterOptions["clearTimeout"];
+};
+
+/** Interpreter.create with default options for convenience */
+export function createInterpreter(options: CreateInterpreterOptions): Interpreter {
+  return Interpreter.create({
+    handleStdout: (stdout: string) => {
+      console.log(stdout);
+    },
+
+    handleEventLoopStatus: (_nPending: number) => {},
+
+    setTimeout: (callback, delayMs) => {
+      return globalThis.setTimeout(callback, delayMs);
+    },
+
+    clearTimeout: (timeoutId) => {
+      globalThis.clearTimeout(timeoutId as number);
+    },
+
+    ...options,
+  } satisfies InterpreterOptions);
+}
