e2dceb70753885556cf18c3258b334fb8b1fbb53
Logan <logan.zartman@utexas.edu>
2026-08-21 22:07:57 -0700

    re-scaffolding for winit ownership of event loop

--- crates/tanaid-tk/src/tk.rs
+++ crates/tanaid-tk/src/tk.rs
@@ -1,37 +1,31 @@
 use crate::tk_context::TkContext;
 use std::rc::Rc;
-use std::time::Duration;
 use tanaid::eval::EvalContext;
 use tanaid::eval_error::EvalError;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::platform::pump_events::{EventLoopExtPumpEvents, PumpStatus};
 
 pub struct Tk {
-  event_loop: winit::event_loop::EventLoop<()>,
-  app: TkApp,
+  context: Rc<TkContext>,
 }
 
 impl Tk {
   pub fn new() -> Self {
     Self {
-      event_loop: winit::event_loop::EventLoop::new().unwrap(),
-      app: TkApp {
-        context: Rc::new(TkContext::new()),
-      },
+      context: Rc::new(TkContext::new()),
     }
   }
 
   pub fn install(&mut self, ctx: &mut EvalContext) -> Result<(), EvalError> {
     {
-      let context = self.app.context.clone();
+      let context = self.context.clone();
       ctx.register_command(
         "canvas",
         Rc::new(move |args, ctx, frame| context.canvas(args, ctx, frame)),
       );
     }
     {
-      let context = self.app.context.clone();
+      let context = self.context.clone();
       ctx.register_command(
         "pack",
         Rc::new(move |args, ctx, frame| context.pack(args, ctx, frame)),
@@ -40,21 +34,26 @@ impl Tk {
 
     Ok(())
   }
-
-  pub fn pump_app_events(&mut self) -> PumpStatus {
-    self
-      .event_loop
-      .pump_app_events(Some(Duration::ZERO), &mut self.app)
-  }
 }
 
-struct TkApp {
+/// Dispatches winit events to a TkContext
+pub struct TkApp {
   context: Rc<TkContext>,
+}
+
+impl TkApp {
+  pub fn new(context: Rc<TkContext>) -> Self {
+    Self { context }
+  }
 }
 
 impl ApplicationHandler for TkApp {
   fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
     self.context.handle_resumed(event_loop);
+  }
+
+  fn about_to_wait(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+    self.context.handle_about_to_wait(event_loop);
   }
 
   fn window_event(
